c3ba261b1e940b9d533d2f393886a4d7db87adf9
Taskmaster01 <alvaro19e@gmail.com>
2023-06-09 20:37:01 -0300

    creacion de CONTROLLER

--- proyecto/models/producto.js
+++ proyecto/models/producto.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 const ProductoSchema = new Schema({
-nombre: {type: String, required: true},//
-descripción:  {type: String, required: true},//String
- imagen:  {type: String, required: true},//String //url de una imagen para mostrar
- precio: {type: Number, required: true},// Number
- stock:  {type: Number, required: true},//Number
- destacado:  {type: Boolean, required: true},//Boolean // solo algunos productos son destacados
+    nombre: { type: String, required: true },//
+    descripción: { type: String, required: true },//String
+    imagen: { type: String, required: true },//String //url de una imagen para mostrar
+    precio: { type: Number, required: true },// Number
+    stock: { type: Number, required: true },//Number
+    destacado: { type: Boolean, required: true },//Boolean // solo algunos productos son destacados
 })
 module.exports = mongoose.models.Producto || mongoose.model('Producto', ProductoSchema);
 
