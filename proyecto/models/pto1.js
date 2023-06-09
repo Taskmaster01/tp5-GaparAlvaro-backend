8c7d09332a495118d58e539f6fd8da7c9b163e8c
Taskmaster01 <alvaro19e@gmail.com>
2023-06-09 18:53:04 -0300

    primer prueba

---
+++ proyecto/models/pto1.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+const Pto1Schema = new Schema({
+nombre: {type: String, required: true},//
+descripción:  {type: String, required: true},//String
+ imagen:  {type: String, required: true},//String //url de una imagen para mostrar
+ precio: {type: Number, required: true},// Number
+ stock:  {type: Number, required: true},//Number
+ destacado:  {type: Boolean, required: true},//Boolean // solo algunos productos son destacados
+})
+module.exports = mongoose.models.Pto1 || mongoose.model('Pto1', AgenteSchema);
