3480f8a11f4e242a550ed624de1a60c1b789e3e8
Taskmaster01 <alvaro19e@gmail.com>
2023-06-09 23:35:57 -0300

    transaccion

--- proyecto/controllers/transaccion.controller.js
+++ proyecto/controllers/transaccion.controller.js
@@ -1,55 +1,65 @@
 const Transaccion = require('../models/transaccion');
 const transaccionCtrl = {}
-transaccionCtrl.gettransaccions = async (req, res) => {
-    var transaccions = await Transaccion.find();
-    res.json(transaccions);
-}
-transaccionCtrl.createTransaccion = async (req, res) => {
+
+
+
+transaccionCtrl.createTansaccion = async (req, res) => {
     var transaccion = new Transaccion(req.body);
     try {
         await transaccion.save();
         res.json({
-            'status': '1',
-            'msg': 'Transaccion guardado.'
-        })
+        'status': '1',
+        'msg': 'Transaccion guardado.'})
     } catch (error) {
         res.status(400).json({
-            'status': '0',
-            'msg': 'Error procesando operacion.'
-        })
+        'status': '0',
+        'msg': 'Error procesando operacion.'})
     }
 }
-transaccionCtrl.getTransaccion = async (req, res) => {
-    const transaccion = await Transaccion.findById(req.params.id);
-    res.json(transaccion);
+
+transaccionCtrl.getTransacciones = async (req, res) => {
+    var transacciones = await Transaccion.find();
+    res.json(transacciones);
 }
-transaccionCtrl.editTransaccion = async (req, res) => {
-    const vtransaccion = new Transaccion(req.body);
-    try {
-        await Transaccion.updateOne({ _id: req.body._id }, vtransaccion);
-        res.json({
-            'status': '1',
-            'msg': 'Transaccion updated'
-        })
-    } catch (error) {
-        res.status(400).json({
-            'status': '0',
-            'msg': 'Error procesando la operacion'
-        })
-    }
+
+transaccionCtrl.getHistoricoEmail = async (req, res) => {
+   // var criteria={'emailCliente': 'hola@gmail.com'};
+   var criteria={};
+    if(req.query.emailCliente!=null && req.query.emailCliente!=""){
+        criteria.emailCliente = { $regex: req.query.emailCliente, $options: "i" }};
+    var transacciones = await Transaccion.find(criteria);
+    res.json(transacciones);
 }
-transaccionCtrl.deleteTransaccion = async (req, res) => {
-    try {
-        await Transaccion.deleteOne({ _id: req.params.id });
-        res.json({
-            status: '1',
-            msg: 'Transaccion removed'
-        })
-    } catch (error) {
-        res.status(400).json({
-            'status': '0',
-            'msg': 'Error procesando la operacion'
-        })
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+transaccionCtrl.getTransaccionesParametros = async (req, res) => {
+   // var criteria={'emailCliente': 'ARS','monedaDestino':'JPN'};
+   var criteria={};
+    if(req.query.emailCliente!=null && req.query.emailCliente!=""){
+      criteria.emailCliente = { $regex: req.query.emailCliente, $options: "i" }};
+    if(req.query.monedaDestino!=null && req.query.monedaDestino!=""){
+      criteria.monedaDestino = { $regex: req.query.monedaDestino, $options: "i" }};
+
+
+    var transacciones = await Transaccion.find(criteria);
+    res.json(transacciones);
 }
+
+
+
 module.exports = transaccionCtrl;
--- proyecto/routes/transaccion.route.js
+++ proyecto/routes/transaccion.route.js
@@ -3,11 +3,12 @@ const transaccionCtrl = require('./../controllers/transaccion.controller');
 //creamos el manejador de rutas
 const express = require('express');
 const router = express.Router();
-//definimos las rutas para la gestion de agente
-router.get('/', transaccionCtrl.gettransaccions);//http://localhost:3000/api/agente/
-router.post('/', transaccionCtrl.createTransacccion);
-router.get('/:id', transaccionCtrl.getTransacccion);
-router.put('/:id', transaccionCtrl.editTransacccion);
-router.delete('/:id', transaccionCtrl.deleteTransacccion);
+//definimos las rutas para la gestion de 
+
+router.post('/', transaccionCtrl.createTansaccion);//Dar de alta una Transaccion
+router.get('/', transaccionCtrl.getTransacciones);//Recuperar TODAS las Transacciones Registradas
+router.get('/emailCliente/', transaccionCtrl.getHistoricoEmail);//-Recuperar el histórico de transacciones de un determinado cliente (GET), utilizar email como clave
+//router.get('/monedaOrigen/monedaDestino', transaccionCtrl.getTransaccionesParametros);
+router.get('/monedas/', transaccionCtrl.getTransaccionesParametros);
 //exportamos el modulo de rutas
 module.exports = router;
